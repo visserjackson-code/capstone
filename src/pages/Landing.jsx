b9a0fae2ea759a08a56b89940d27758d02c4141a
visserjackson-code <jcv74@proton.me>
2026-05-19 19:17:59 -0400

    updated landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,10 @@ function Landing () {
 
     return (
         <>
-        <h1 className="landingHeader">Pokémon Team Tracker</h1>
+        <h1 className="landing-header">Pokémon Team Tracker</h1>
+        <p className="landing-desc">This page allows you to track your Pokémon teams across the mainline series games --
+            from Red and Blue all the way up to Scarlet and Violet. 
+        </p>
         </>
     )
 }
