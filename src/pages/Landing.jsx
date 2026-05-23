57720cd2f0f23686fb926538998124941712f9a7
visserjackson-code <jcv74@proton.me>
2026-05-23 11:49:45 -0400

    somewhat decent landing page, still need to center buttons

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,36 @@
-function Landing () {
-
-    return (
-        <>
-        <h1 className="landing-header">Pokémon Team Tracker</h1>
-        <p className="landing-desc">This page allows you to track your Pokémon teams across the mainline series games --
-            from Red and Blue all the way up to Scarlet and Violet. 
-        </p>
-        </>
-    )
+function Landing() {
+  return (
+    <>
+    <div className="page-background">
+        <div className="glass-box">
+      <h1 className="landing-header">Pokémon Team Tracker</h1>
+      <p className="landing-desc">
+        Track your Pokémon teams across the mainline series games, from Red and
+        Blue all the way up to Scarlet and Violet! Interested in{" "}
+        <a href="https://en.wikipedia.org/wiki/Nuzlocke" target="_blank">
+          Nuzlockes
+        </a>
+        ? Check out the Nuzlocke zone for detailed run tracking.
+      </p>
+      <button class="button">
+        <div class="button-outer">
+          <div class="button-inner">
+            <span>Track teams</span>
+          </div>
+        </div>
+      </button>
+      <span>OR</span>
+      <button class="button">
+        <div class="button-outer">
+          <div class="button-inner">
+            <span>Nuzlocke zone</span>
+          </div>
+        </div>
+      </button>
+      </div>
+      </div>
+    </>
+  );
 }
 
 export default Landing;
