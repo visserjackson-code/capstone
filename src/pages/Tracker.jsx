7a9c93c6cc3875c503919c78d53aa6333c30137b
visserjackson-code <jcv74@proton.me>
2026-05-23 14:58:59 -0400

    added button mockup

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import Pokemon from "../components/Pokemon";
 
 function Tracker() {
+
+    const [input, setInput] = useState('');
+    const [search, setSearch] = useState('');
+
     return (
         <>
-        <h1>Tracker page</h1>
-        <Pokemon name="pikachu"/>
+        <h1>Search for a Pokémon</h1>
+        <div>
+            <input 
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Enter a Pokémon name"
+            />
+
+            <button className="tracker-button" onClick={() => setSearch(input)}>Search</button>
+
+            {search && <Pokemon name={search} />}
+        </div>
+
+        <button class="button">
+        <div class="button-outer">
+          <div class="button-inner">
+            <span>Add to Team</span>
+          </div>
+        </div>
+      </button>
         </>
     )
 }
--- src/utils/pokeapi.js
+++ src/utils/pokeapi.js
@@ -1,7 +1,7 @@
 export const getSprite = async (mon) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${mon.toLowerCase()}`);
     const data = await response.json();
-    const sprite =  data.sprites.other.showdown.front_default;
+    const sprite =  data.sprites.other.showdown.front_default ?? data.sprites.front_default;
 
     return sprite;
 }  
