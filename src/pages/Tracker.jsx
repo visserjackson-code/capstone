854a41d7a452aa253e0b5bb25a9d69e59a6997c2
visserjackson-code <jcv74@proton.me>
2026-05-24 10:22:05 -0400

    basic tracker works

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -1,30 +1,40 @@
 import { useState } from "react";
-import Pokemon from "../components/Pokemon";
 import Team from "../components/Team";
 
 function Tracker() {
+    const [input, setInput] = useState("");
+    const [team, setTeam] = useState(Array(6).fill(null));
+    const [activeSlot, setActiveSlot] = useState(null);
 
-    // const [input, setInput] = useState('');
-    // const [search, setSearch] = useState('');
+    const handleSlotClick = (index) => {
+        setActiveSlot(index);
+    };
+
+    const handleSearch = () => {
+        if (input && activeSlot !== null) {
+            const newTeam = [...team];
+            newTeam[activeSlot] = input.toLowerCase();
+            setTeam(newTeam);
+            setActiveSlot(null);
+            setInput("");
+        }
+    };
 
     return (
         <>
-        {/* <h1>Search for a Pokémon</h1>
+        <h1 className="tracker-header">Add a Pokémon</h1>
         <div>
-            <input 
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Enter a Pokémon name"
-            />
-
-            <button className="tracker-button" onClick={() => setSearch(input)}>Search</button>
-
-            {search && <Pokemon name={search} />}
+            <input
+             type="text"
+             value={input}
+             onChange={(e) => setInput(e.target.value)}
+             placeholder="Enter a Pokémon name" 
+             />
+             <button className="search-button" onClick={handleSearch}>Click to add Pokemon</button>
         </div>
-        <Team/> */}
+        <Team team={team} activeSlot={activeSlot} onSlotClick={handleSlotClick} />
         </>
-    )
+    );
 }
 
 
