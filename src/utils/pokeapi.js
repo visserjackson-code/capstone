ff079fcf3f530ec4852e355deebaed31caa72441
visserjackson-code <jcv74@proton.me>
2026-05-26 15:40:11 -0400

    refactored getPokemon function to fetch type(s) and stats

--- src/utils/pokeapi.js
+++ src/utils/pokeapi.js
@@ -1,10 +1,16 @@
-export const getSprite = async (mon) => {
+export const getPokemon = async (mon) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${mon.toLowerCase()}`);
     const data = await response.json();
-    const sprite =  data.sprites.other.showdown.front_default ?? data.sprites.front_default;
 
-    return sprite;
-}  
+    return {
+        sprite: data.sprites.other.showdown.front_default ?? data.sprites.front_default,
+        types: data.types.map(t => t.type.name),
+        stats: data.stats.reduce((acc, s) => {
+            acc[s.stat.name] = s.base_stat;
+            return acc;
+        }, {})
+    };
+} ; 
 
 
 
