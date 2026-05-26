2553d228e0bae0eadd85e69037181af22ebdf665
visserjackson-code <jcv74@proton.me>
2026-05-26 16:09:52 -0400

    basic hover works with overlay. need to style

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,23 +1,37 @@
-import { getPokemon } from "../utils/pokeapi";
-import { useState, useEffect } from "react";
+import {getPokemon} from "../utils/pokeapi";
+import {useState, useEffect} from "react";
+import StatsOverlay from "./StatsOverlay";
 
+function Pokemon({name}) {
+  const [pokemon, setPokemon] = useState(null);
+  const [isHovered, setIsHovered] = useState(false);
 
-function Pokemon ({name}) {
+  useEffect(() => {
+    getPokemon(name).then((data) => setPokemon(data));
+  }, [name]);
 
-    const [pokemon, setPokemon] = useState(null);
+  if (!pokemon) return <p>Loading..</p>;
 
-    useEffect(() => {
-        getPokemon(name).then(data => setPokemon(data));
-    }, [name]);
-
-    if (!pokemon) return <p>Loading..</p>
-    return (
-        <>
+  return (
+    <>
+      <div
+        className="pokemonWrapper"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <div className="sprite-img">
-            <img src={pokemon.sprite} alt={name}></img>
+          <img src={pokemon.sprite} alt={name}></img>
+          {isHovered && (
+            <StatsOverlay
+              name={name}
+              types={pokemon.types}
+              stats={pokemon.stats}
+            />
+          )}
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default Pokemon;
