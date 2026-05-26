bfebe761e2927eb7c575596e2f4078f4c3c50e0f
visserjackson-code <jcv74@proton.me>
2026-05-26 16:34:24 -0400

    hovering overlay mostly fixed

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,11 +1,11 @@
 import {getPokemon} from "../utils/pokeapi";
 import {useState, useEffect} from "react";
 import StatsOverlay from "./StatsOverlay";
-import "../styles/Pokemon.css"
+import "../styles/Pokemon.css";
 
 function Pokemon({name}) {
   const [pokemon, setPokemon] = useState(null);
-  const [isHovered, setIsHovered] = useState(false);
+  //   const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
     getPokemon(name).then((data) => setPokemon(data));
@@ -15,21 +15,9 @@ function Pokemon({name}) {
 
   return (
     <>
-      <div
-        className="pokemonWrapper"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <div className="sprite-img">
-          <img src={pokemon.sprite} alt={name}></img>
-          {isHovered && (
-            <StatsOverlay
-              name={name}
-              types={pokemon.types}
-              stats={pokemon.stats}
-            />
-          )}
-        </div>
+      <div className="pokemon-wrapper">
+        <img src={pokemon.sprite} alt={name} />
+        <StatsOverlay name={name} types={pokemon.types} stats={pokemon.stats} />
       </div>
     </>
   );
