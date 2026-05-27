6fccbc4404f5ce4e91df7aa4b0ef2e31c8079c57
visserjackson-code <jcv74@proton.me>
2026-05-26 20:21:00 -0400

    testing rendering for encounter

--- src/pages/Nuzlocke.jsx
+++ src/pages/Nuzlocke.jsx
@@ -1,7 +1,17 @@
-function Nuzlocke () {
-    return (
-        <h1>Nuzlocke</h1>
-    )
+import Encounter from "../components/Encounter";
+
+function Nuzlocke() {
+  const testEncounter = { id: 1, pokemon: 'pidgey', location: 'Route 1', alive: true };
+
+  return (
+    <div>
+      <Encounter
+        encounter={testEncounter}
+        onToggle={(id) => console.log('toggle', id)}
+        onDelete={(id) => console.log('delete', id)}
+      />
+    </div>
+  );
 }
 
 export default Nuzlocke;
