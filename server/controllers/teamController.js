90ec96cb83a7451e3d043765be9d7662776fb4a6
visserjackson-code <jcv74@proton.me>
2026-05-30 13:04:10 -0400

    added controller to get and update teams

---
+++ server/controllers/teamController.js
@@ -0,0 +1,26 @@
+import Team from "../models/Team";
+
+export const getTeams = async (req, res) => {
+  try {
+    const teams = await Team.find({userId: req.userId});
+    res.status(200).json(teams);
+  } catch (err) {
+    res.status(500).json({message: "Server error", error: err.message});
+  }
+};
+
+export const saveTeam = async (req, res) => {
+  const {game, slots} = req.body;
+
+  try {
+    const team = await Team.findOneAndUpdate(
+      {userId: req.userId, game},
+      {slots},
+      {upsert: true, new: true},
+    );
+
+    res.status(200).json(team);
+  } catch (err) {
+    res.status(500).json({message: "Server error", error: err.message});
+  }
+};
