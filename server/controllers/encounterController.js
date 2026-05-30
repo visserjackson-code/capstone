a7a8abcbd1aa4523a62b950ea365936281097cf6
visserjackson-code <jcv74@proton.me>
2026-05-30 14:01:57 -0400

    added crud controller features for encounters

---
+++ server/controllers/encounterController.js
@@ -0,0 +1,57 @@
+import Encounter from "../models/Encounter";
+
+export const getEncounters = async (req, res) => {
+  try {
+    const encounters = await Encounter.find({
+      userId: req.userId,
+      game: req.params.game,
+    });
+    res.status(200).json(encounters);
+  } catch (err) {
+    res.status(500).json({message: "Server error", error: err.message});
+  }
+};
+
+export const addEncounter = async (req, res) => {
+  const {game, pokemon, nickname, location} = req.body;
+
+  try {
+    const encounter = await Encounter.create({
+      userId: req.userId,
+      game,
+      pokemon,
+      nickname,
+      location,
+      alive: true,
+    });
+    res.status(201).json(encounter);
+  } catch (err) {
+    res.status(500).json({message: "Server error", error: err.message});
+  }
+};
+
+export const toggleEncounter = async (req, res) => {
+  try {
+    const encounter = await Encounter.findOne({
+      _id: req.params.id,
+      userId: req.userId,
+    });
+    if (!encounter)
+      return res.status(404).json({message: "Encounter not found"});
+
+    encounter.alive = !encounter.alive;
+    await encounter.save();
+    res.status(200).json(encounter);
+  } catch (err) {
+    res.status(500).json({message: "Server error", error: err.message});
+  }
+};
+
+export const deleteEncounter = async (req, res) => {
+  try {
+    await Encounter.findOneAndDelete({_id: req.params.id, userId: req.userId});
+    res.status(200).json({message: "Deleted"});
+  } catch (err) {
+    res.status(500).json({message: "Server error", error: err.message});
+  }
+};
